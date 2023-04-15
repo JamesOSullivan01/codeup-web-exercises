79b6f4654cbaa33355566dd37b61ea93d1293581
James OSullivan <james.a.osullivan01@gmail.com>
2023-04-15 07:20:42 -0500

    Latest Changes

--- js/external.js
+++ js/external.js
@@ -1,74 +1,76 @@
-"use strict"
-console.log("Hello from external JS");
-alert("Welcome to my website");
-var userInput = prompt('What is your fav color:');
-alert('Great! My fav color is: ' + userInput + ' too!');
-
-let userInputTwo = prompt('How old are you');
-console.log('wow ' + userInputTwo + ' is old');
-
-let userInputLm = parseInt(prompt('How many days would you like to rent the little mermaid'));
-alert('You are renting The little mermaid for ' + userInputLm + ' days');
-
-let userInputBb = parseInt(prompt("How many days would you like to rent the Brother Bear?"));
-alert('You are renting Brother Bear for ' + userInputBb + ' days');
-
-let userInputHer = parseInt(prompt("How many days would you like to rent Hercules?"));
-alert('You are renting Herculus for ' + userInputHer + ' days');
-
-let costPerMovies = 3;
-let totalCost = userInputLm + userInputBb + userInputHer * costPerMovies
-
-alert("Your cost for these rentals is: " + totalCost);
-
-// def used days instead of hours :/
-// Needlessly console.logged a bunch of things.
-
-let googlePay = parseInt(prompt('Google is paying me:'));
-console.log('Google is paying me: ' + googlePay);
-let googleDays = parseInt(prompt('I worked for Google for how many days?'));
-console.log('I worked for Google for ' + googleDays + ' days.');
-
-let amazonPay = parseInt(prompt('Amazon is paying me:'));
-console.log('Amazon is paying me: ' + amazonPay);
-let amazonDays = parseInt(prompt('I worked for Amazon for how many days?'));
-console.log('I worked for Amazon for ' + amazonDays + ' days.');
-
-let fbPay = parseInt(prompt('Facebook is paying me:'));
-console.log('Facebook is paying me: ' + fbPay);
-let fbDays = parseInt(prompt('I worked for Facebook for how many days?'));
-console.log('I worked for Facebook for ' + fbDays + ' days.');
-
-let totalPay = (googlePay * googleDays) + (amazonPay * amazonDays) + (fbPay * fbDays);
-
-alert('You made a total of $' + totalPay + ' from Google, Amazon, and Facebook.');
-
-
-let classFull = confirm('Does this class have an available seat? ')
-let noConflict = confirm('Are you sure you can attend this class?');
-if (!classFull && !noConflict) {
-    console.log("Student can enroll");
-} else {
-    console.log('cannot enter')
-}
-
-let itemsBought = parseInt(prompt('How many items did you buy? '));
-let offerExpired = confirm('is offer expired? ')
-let isPremium = confirm('Are you a premium member? ');
-
-if ((itemsBought > 2 || isPremium) && !offerExpired) {
-    alert("Product offer applied");
-} else {
-    alert("can't apply");
-}
-
-var username = 'codeup';
-var password = 'notastrongpassword';
-
-let isPasswordValid = password.length >= 5;
-let doesNotContainUsername = !password.includes(username);
-let usernameBeLessThan20 = username.length <= 20;
-let isWhiteSpaceFree = username.trim() === username && password.trim() === password;
+// "use strict"
+// console.log("Hello from external JS");
+// alert("Welcome to my website");
+// var userInput = prompt('What is your fav color:');
+// alert('Great! My fav color is: ' + userInput + ' too!');
+//
+// let userInputTwo = prompt('How old are you');
+// console.log('wow ' + userInputTwo + ' is old');
+//
+// let userInputLm = parseInt(prompt('How many days would you like to rent the little mermaid'));
+// alert('You are renting The little mermaid for ' + userInputLm + ' days');
+//
+// let userInputBb = parseInt(prompt("How many days would you like to rent the Brother Bear?"));
+// alert('You are renting Brother Bear for ' + userInputBb + ' days');
+//
+// let userInputHer = parseInt(prompt("How many days would you like to rent Hercules?"));
+// alert('You are renting Herculus for ' + userInputHer + ' days');
+//
+// let costPerMovies = 3;
+// let totalCost = userInputLm + userInputBb + userInputHer * costPerMovies
+//
+// alert("Your cost for these rentals is: " + totalCost);
+//
+// // def used days instead of hours :/
+// // Needlessly console.logged a bunch of things.
+//
+// let googlePay = parseInt(prompt('Google is paying me:'));
+// console.log('Google is paying me: ' + googlePay);
+// let googleDays = parseInt(prompt('I worked for Google for how many days?'));
+// console.log('I worked for Google for ' + googleDays + ' days.');
+//
+// let amazonPay = parseInt(prompt('Amazon is paying me:'));
+// console.log('Amazon is paying me: ' + amazonPay);
+// let amazonDays = parseInt(prompt('I worked for Amazon for how many days?'));
+// console.log('I worked for Amazon for ' + amazonDays + ' days.');
+//
+// let fbPay = parseInt(prompt('Facebook is paying me:'));
+// console.log('Facebook is paying me: ' + fbPay);
+// let fbDays = parseInt(prompt('I worked for Facebook for how many days?'));
+// console.log('I worked for Facebook for ' + fbDays + ' days.');
+//
+// let totalPay = (googlePay * googleDays) + (amazonPay * amazonDays) + (fbPay * fbDays);
+//
+// alert('You made a total of $' + totalPay + ' from Google, Amazon, and Facebook.');
+//
+//
+// let classFull = confirm('Does this class have an available seat? ')
+// let noConflict = confirm('Are you sure you can attend this class?');
+// if (!classFull && !noConflict) {
+//     console.log("Student can enroll");
+// } else {
+//     console.log('cannot enter')
+// }
+//
+// let itemsBought = parseInt(prompt('How many items did you buy? '));
+// let offerExpired = confirm('is offer expired? ')
+// let isPremium = confirm('Are you a premium member? ');
+//
+// if ((itemsBought > 2 || isPremium) && !offerExpired) {
+//     alert("Product offer applied");
+// } else {
+//     alert("can't apply");
+// }
+//
+// var username = 'codeup';
+// var password = 'notastrongpassword';
+//
+// let isPasswordValid = password.length >= 5;
+// let doesNotContainUsername = !password.includes(username);
+// let usernameBeLessThan20 = username.length <= 20;
+// let isWhiteSpaceFree = username.trim() === username && password.trim() === password;
+//
+//
 
 
 
