b89c7a3ca54c27e2fcb4185d555afc5a0db10afa
James OSullivan <james.a.osullivan01@gmail.com>
2023-04-19 16:45:49 -0500

    Completed for loops questions

---
+++ js/for_loops.js
@@ -0,0 +1,37 @@
+// https://www.programiz.com/javascript/examples/multiplication-table
+function showMultiplicationTable() {
+    let number = 7
+    for (let i = 1; i < 11; i++) {
+        let result = i * number;
+        console.log(`${number} * ${i} = ${result}`);
+    }
+}
+
+showMultiplicationTable();
+
+
+for (let i = 10; i > 0; i--) {
+    let randomNumber = Math.floor(Math.random() * (200 - 20 + 1)) + 20;
+    if (randomNumber % 2 === 0) {
+        console.log(randomNumber + "is even");
+    } else {
+        console.log(randomNumber + "is odd");
+    }
+}
+// https://www.tutorialstonight.com/number-pattern-programs-in-javascript
+let n = 9;
+let string = "";
+// External loop
+for (let i = 1; i <= n; i++) {
+    // Internal loop
+    for (let j = 1; j <= i; j++) {
+        string += i;
+    }
+    string += "\n";
+}
+console.log(string);
+
+for (let i = 100; i > 0; i -= 5) {
+    console.log(i);
+}
+
