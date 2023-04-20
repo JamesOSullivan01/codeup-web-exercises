6ea674ac86dcde6f001e40e59187410e2a1dacd6
James OSullivan <james.a.osullivan01@gmail.com>
2023-04-20 12:01:12 -0500

    Completed while loop exercises.

--- js/while.js
+++ js/while.js
@@ -7,3 +7,24 @@ while (result < 66000) {
     i++;
     result *= 2;
 }
+
+
+let allCones = Math.floor(Math.random() * 50) + 50;
+let remainingCones = allCones;
+
+console.log("Starting with " + allCones + " cones.");
+
+do {
+    let conesBought = Math.floor(Math.random() * 5) + 1;
+    console.log("Cones bought: " + conesBought);
+
+    if (conesBought > remainingCones) {
+        console.log("Cannot sell you " + conesBought + " cones, I only have " + remainingCones + "...");
+    } else {
+        remainingCones -= conesBought;
+        console.log(remainingCones + " cones left...");
+    }
+
+} while (remainingCones > 0);
+
+console.log("Yay! I sold them all!");
