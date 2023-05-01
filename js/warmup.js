340fb6d20196deede433b9a631ee9116511ce929
James OSullivan <james.a.osullivan01@gmail.com>
2023-05-01 16:08:31 -0500

    Finished js warm up, finsihed forms.html exercise.

--- js/warmup.js
+++ js/warmup.js
@@ -5,3 +5,27 @@ function thisIsTheNumber7 () {
 
 console.log(thisIsTheNumber7());
 
+// // Write a function that takes in a number and returns an array of numbers that are divisible within the argument. If the value provided is not a number, the function should return false.
+
+// https://www.w3resource.com/javascript-exercises/javascript-function-exercise-13.php
+let numberToFactor = 200;
+    function findFactors (input) {
+    if (typeof input === 'number' && !isNaN(input)) {
+
+        let factors = [];
+        // Loop through all numbers from 1 to input/2 and check if they are a divisor
+        for (let i = 1; i <= input / 2; i++) {
+            if (input % i === 0) {
+                factors.push(i);
+            }
+        }
+
+        // Add input as a factor
+        factors.push(input);
+
+        return factors;
+} else {
+    return false}
+    }
+
+console.log(findFactors(numberToFactor));
