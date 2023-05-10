530f294cb4fc5212c60da16d7a57e1488f69f86c
James OSullivan <james.a.osullivan01@gmail.com>
2023-05-10 16:55:03 -0500

    Completed warm-up and bootstrap pizza exercise.

--- js/warmup.js
+++ js/warmup.js
@@ -117,7 +117,30 @@ do {
 console.log(i);
 
 
+// Write a function that takes in an array of numbers and returns all of the numbers added together (We'll be assuming that only arrays are going to be used with this function).
 
+function addEmUp(arr) {
+    if (typeof arr !== 'object') {
+        return false
+    }
+    let addedArr = 0;
+    // since we are summing up numbers this variable needs to be sewt to zero and not an array
+    // arr.forEach(function(arrOfNumbers) {
+    //     addedArr += arrOfNumbers
+    // });
+    for(let i = 0; i < arr.length; i++) {
+        addedArr += arr[i]
+        //There is no need to parsefloat these as they are already numbers
+    }
+    return addedArr;
+};
+
+console.log(addEmUp([2,6,19])); // returns 27
+console.log(addEmUp([-99, 180, -5])); // returns 76
+console.log(addEmUp([44,10,7])); // returns 61
+console.log(addEmUp([-100])); // returns -100
+console.log(addEmUp([1,2,3,4,5,6,7,8,9,10])); // returns 55
+console.log(addEmUp([-13, -92, -3500])) // returns -3605
 
 
 
