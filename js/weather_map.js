3a664e2bbec0a8c7abafac1f744c3dca277890f9
James OSullivan <james.a.osullivan01@gmail.com>
2023-05-31 14:01:27 -0500

    Added cards above map.

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,11 +1,23 @@
 "use strict"
-$( document ).ready(function() {
-
-    $.get(`https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${OMW_KEY}`).done(function(data) {
+$(document).ready(function () {
+    let latLong = [29.7604, -95.3698];
+    $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${latLong[0]}&lon=${latLong[1]}&units=imperial&appid=${OMW_KEY}`).done(function (data) {
         console.log(data);
+
+        let todayDate = new Date(data.list[0].dt * 1000);
+        let day = todayDate.getDay()
+        let dateTest = `<section><h1>${day}</h1></section>`
+
+        $('#date_test').html(todayDate)
+
+        mapboxgl.accessToken = myMapboxKey;
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11',
+            zoom: 8,
+            center: [-95.473488, 30.315239]
+        });
+
     });
-
-
-
-
 });
+
