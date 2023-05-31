854fa9159d0fdb01c4ef481a89dd361c1a03ab61
James OSullivan <james.a.osullivan01@gmail.com>
2023-05-31 14:28:17 -0500

    added a for loop to iterate over my cards and push them into my bootstrap cards.

--- js/weather_map.js
+++ js/weather_map.js
@@ -4,11 +4,20 @@ $(document).ready(function () {
     $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${latLong[0]}&lon=${latLong[1]}&units=imperial&appid=${OMW_KEY}`).done(function (data) {
         console.log(data);
 
-        let todayDate = new Date(data.list[0].dt * 1000);
-        let day = todayDate.getDay()
-        let dateTest = `<section><h1>${day}</h1></section>`
 
-        $('#date_test').html(todayDate)
+
+        let dateTitles = $('.title-for-card');
+
+        for (let i = 0; i < dateTitles.length; i++) {
+            let forecastIndex = i * 8; // Each forecast entry is for a 3-hour interval, so we increment by 8 to get the next day's forecast
+            let todayDate = new Date(data.list[forecastIndex].dt * 1000);
+            let day = todayDate.toLocaleDateString();
+
+            $(dateTitles[i]).html(day);
+        }
+
+
+
 
         mapboxgl.accessToken = myMapboxKey;
         var map = new mapboxgl.Map({
@@ -16,6 +25,11 @@ $(document).ready(function () {
             style: 'mapbox://styles/mapbox/streets-v11',
             zoom: 8,
             center: [-95.473488, 30.315239]
+
+
+
+
+
         });
 
     });
