baa0442e87f8b4ba463691155cb496ab583985cb
James OSullivan <james.a.osullivan01@gmail.com>
2023-06-01 11:59:29 -0500

    Added geocoder to map.

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,4 +1,3 @@
-"use strict"
 $(document).ready(function () {
     let latLong = [29.7604, -95.3698];
     $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${latLong[0]}&lon=${latLong[1]}&units=imperial&appid=${OMW_KEY}`).done(function (data) {
@@ -52,30 +51,33 @@ $(document).ready(function () {
         for (let i = 0; i < windElements.length; i++) {
             let forecastIndex = i * 8;
             let wind = data.list[forecastIndex].wind;
-            let htmlString = `Speed: ${wind.speed} MPH <br> Direction: ${wind.deg}&deg;`;
+            let htmlString = ` Wind Speed: ${wind.speed} MPH <br> Wind Direction: ${wind.deg}&deg;`;
             $(windElements[i]).html(htmlString);
         }
+    }).fail(function () {
+        console.log('Failed to fetch weather data');
+    });
 
-        let sunriseSunsetElements = $('.sunInfo');
+    mapboxgl.accessToken = myMapboxKey;
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        zoom: 8,
+        center: [-95.473488, 30.315239]
+    });
 
-        for (let i = 0; i < sunriseSunsetElements.length; i++){
-            let forecastIndex = i * 8;
+    // Add the control to the map.
+    const geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    });
+    document.getElementById('map').appendChild(geocoder.onAdd(map));
 
-        }
-
-
-
-
-        mapboxgl.accessToken = myMapboxKey;
-        var map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v11',
-            zoom: 8,
-            center: [-95.473488, 30.315239]
-
-
+    // Add event listener to move the map when an address is entered
+    geocoder.on('result', function (e) {
+        map.flyTo({
+            center: e.result.center,
+            zoom: 13
         });
-
     });
 });
-
