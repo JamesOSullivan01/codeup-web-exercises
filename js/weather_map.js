6a18213050e020b956d91a55ff3b6c411da37547
James OSullivan <james.a.osullivan01@gmail.com>
2023-06-01 10:11:57 -0500

    Finsihed description li and wind li for weather cards

--- js/weather_map.js
+++ js/weather_map.js
@@ -4,16 +4,63 @@ $(document).ready(function () {
     $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${latLong[0]}&lon=${latLong[1]}&units=imperial&appid=${OMW_KEY}`).done(function (data) {
         console.log(data);
 
-
+        // Date titles of cards
 
         let dateTitles = $('.title-for-card');
+        let forecastIndex;
 
         for (let i = 0; i < dateTitles.length; i++) {
-            let forecastIndex = i * 8; // Each forecast entry is for a 3-hour interval, so we increment by 8 to get the next day's forecast
+            let forecastIndex = i * 8; // Each forecast entry is for a 3-hour interval, so I increment by 8 to get the next day's forecast
             let todayDate = new Date(data.list[forecastIndex].dt * 1000);
+            // https://www.w3schools.com/jsref/jsref_tolocaledatestring.asp
             let day = todayDate.toLocaleDateString();
 
             $(dateTitles[i]).html(day);
+        }
+
+        // Temperate and icons for cards
+
+        let temperatureElements = $('.temperature');
+
+        for (let i = 0; i < temperatureElements.length; i++) {
+            let forecastIndex = i * 8;
+            let temperature = data.list[forecastIndex].main.temp;
+            let iconCode = data.list[forecastIndex].weather[0].icon;
+            let iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
+            // https://developer.mozilla.org/en-US/docs/Glossary/Entity
+            let htmlString = `<img src="${iconUrl}" alt="Weather Icon"> ${temperature.toFixed(0)}&deg`;
+            $(temperatureElements[i]).html(htmlString);
+        }
+
+        // Description li for cards
+
+        let descriptionElements = $('.description');
+
+        for (let i = 0; i < descriptionElements.length; i++) {
+            let forecastIndex = i * 8;
+            let description = data.list[i].weather[0].description;
+            let humidity = data.list[i].main.humidity;
+            let htmlString = `Description: ${description} <div></div> Humidity: ${humidity}%`;
+            $(descriptionElements[i]).html(htmlString);
+
+        }
+
+        // Wind li for cards
+
+        let windElements = $('.wind');
+
+        for (let i = 0; i < windElements.length; i++) {
+            let forecastIndex = i * 8;
+            let wind = data.list[forecastIndex].wind;
+            let htmlString = `Speed: ${wind.speed} MPH <br> Direction: ${wind.deg}&deg;`;
+            $(windElements[i]).html(htmlString);
+        }
+
+        let sunriseSunsetElements = $('.sunInfo');
+
+        for (let i = 0; i < sunriseSunsetElements.length; i++){
+            let forecastIndex = i * 8;
+
         }
 
 
@@ -25,9 +72,6 @@ $(document).ready(function () {
             style: 'mapbox://styles/mapbox/streets-v11',
             zoom: 8,
             center: [-95.473488, 30.315239]
-
-
-
 
 
         });
