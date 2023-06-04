9dd60299e61f6d3d41e98fdd48f11d8e23cb8561
James OSullivan <james.a.osullivan01@gmail.com>
2023-06-04 16:23:32 -0500

    Completed problem 5 of practice DOM assessment.

--- js/main.js
+++ js/main.js
@@ -31,6 +31,18 @@ $('#dark-mode-btn').on('click', function(){
         $('#counter').text(counter);
     }, 1000);
 
+    // When the user clicks on the button (<button id="list-chars-btn">), take any text the user has typed in the input field (<input id="characters">), and output each individual character as a separate list item within the unordered list (<ul id="characters-list">). Clicking the button multiple times should not append additional list items.
+
+    $('#list-chars-btn').on('click', function(e) {
+        e.preventDefault()
+        var inputText = $('#characters').val();
+        $('#characters-list').empty();
+
+        for (var i = 0; i < inputText.length; i++) {
+            var newLi = $('<li></li>').text(inputText[i]);
+            $('#characters-list').append(newLi);
+        }
+    });
 
 
 });
