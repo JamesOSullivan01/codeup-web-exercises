9a578a1f31809950a49305f4a0af13781bd05838
James OSullivan <james.a.osullivan01@gmail.com>
2023-06-04 17:46:54 -0500

    Completed problem 6 of DOM assessment practice.

--- js/main.js
+++ js/main.js
@@ -33,16 +33,31 @@ $('#dark-mode-btn').on('click', function(){
 
     // When the user clicks on the button (<button id="list-chars-btn">), take any text the user has typed in the input field (<input id="characters">), and output each individual character as a separate list item within the unordered list (<ul id="characters-list">). Clicking the button multiple times should not append additional list items.
 
-    $('#list-chars-btn').on('click', function(e) {
-        e.preventDefault()
-        var inputText = $('#characters').val();
-        $('#characters-list').empty();
 
-        for (var i = 0; i < inputText.length; i++) {
-            var newLi = $('<li></li>').text(inputText[i]);
-            $('#characters-list').append(newLi);
-        }
-    });
+
+        $('#clean-animal-list-btn').click(function() {
+            // Iterate over each <li> element with the class "animal-list-item" within the "animals-list" <ul>
+            $('#animals-list li.animal-list-item').each(function() {
+                // Get the text content of the current <li> element
+                var animal = $(this).text();
+
+                // Select the next sibling <li> element and remove it from the DOM
+                var nextItem = $(this).next('li').remove();
+
+                // Create a new <li> element with the "This is a..." sentence and insert it after the current <li>
+                $('<li>').text('This is a... ' + animal).insertAfter($(this));
+
+                // Move the current <li> element to the next line by inserting it after the removed <li>
+                $(this).insertAfter(nextItem).remove();
+            });
+        });
+
+
+
+
+
+
+
 
 
 });
